6750f44824eb9649ad875567a1a344808f0b1c67
Blarg <bobby1821@hotmail.com>
2025-12-27 21:00:36 -0500

    Modals are too confusing...

---
+++ src/commands/Utility/slashcommand-set-logs.js
@@ -0,0 +1,42 @@
+const { ChatInputCommandInteraction, ChannelSelectMenuInteraction } = require('discord.js');
+const DiscordBot = require('../../client/DiscordBot');
+const ApplicationCommand = require('../../structure/ApplicationCommand');
+
+module.exports = new ApplicationCommand({
+    command: {
+        name: 'set-logs',
+        description: 'Set the log channel for the server.',
+        type: 1,
+        options: [
+            {
+                name: 'channel',
+                description: 'The channel to set as log channel.',
+                type: 7,
+                required: true
+            }
+        ]
+    },
+    options: { cooldown: 5000 },
+    /**
+     * @param {DiscordBot} client
+     * @param {ChatInputCommandInteraction} interaction
+     */
+    run: async (client, interaction) => {
+        const channel = interaction.options.getChannel('channel');
+
+        if (!interaction.guildId) {
+            await interaction.reply({ content: 'This command can only be used in a server.', ephemeral: true });
+            return;
+        }
+
+        if (!channel || channel.type !== 0) { // 0 is GUILD_TEXT
+            await interaction.reply({ content: 'Please select a valid text channel.', ephemeral: true });
+            return;
+        }
+
+        //const key = `logchannel-${interaction.guildId}`;
+        //client.database.set(key, channel.id);
+
+        await interaction.reply({ content: `Log channel has been set to ${channel}.`, ephemeral: true });
+    }
+}).toJSON();
